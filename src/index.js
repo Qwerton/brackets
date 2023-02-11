1f91889484b198499f82eefda631874c2be1b99d
Qwerton <Qwerton@users.noreply.github.com>
2023-02-11 10:51:16 +0300

    upd: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const configArray = [];
+  bracketsConfig.forEach(item => configArray.push(item.join('')));
+  while (true) {
+    let x = 0;
+    configArray.forEach(item => str.includes(item) ? str = str.replace(item, '') : x++);
+    if(x === configArray.length) break;
+  }
+  if (str.length === 0) return true;
+  return false;
 }
